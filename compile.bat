5e37d56626d7440eb83d6d24bddd17b4e488d0ac
Mitesh Goplani <miteshgoplani@gmail.com>
2018-05-08 00:23:39 +0530

    Added major functionalities

---
+++ compile.bat
@@ -0,0 +1,6 @@
+set path="C:\Program Files (x86)\Java\jdk1.7.0_79\bin"
+javac -d . EncryptDecrypt.java
+javac -d . FileUtility.java
+javac -d . MenuMain.java
+java codeperfect.javaproject.filemanager.MenuMain
+pause
